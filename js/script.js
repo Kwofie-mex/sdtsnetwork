67c642232f46048d194a928ca5c98e3ff48885d1
jkwofie <judekwofie2357@gmail.com>
2020-10-22 16:37:57 +0000

    finished header

--- js/script.js
+++ js/script.js
@@ -7,7 +7,8 @@ document.addEventListener("DOMContentLoaded",function (event) {
 	//Dropdown Menu function
 	document.querySelector("#bar").addEventListener("click",function (event){
 			
-			if (document.querySelector("nav").style.display==="none") {
+
+			if (getComputedStyle(document.querySelector("nav")).display==="none") {
 				getComputedStyle(document.querySelector("#logo"))
 				document.querySelector("nav").style.top=getComputedStyle(document.querySelector("header")).height;
 
